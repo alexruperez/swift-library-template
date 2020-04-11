731ed57763b9b161543618e60212006bd441cf7b
Alex Rupérez <me@alexruperez.com>
2020-04-11 05:35:55 +0200

    Library name taken from origin repo name if not provided.

--- generate.sh
+++ generate.sh
@@ -1,20 +1,24 @@
 if [ -z "$1" ]
 then
-  echo "Remember to specify the name of your library."
-  exit 1
+  origin=$(git remote get-url origin)
+  end=${origin##*/}
+  unsafe=${end%.*}
+  name=$(echo $unsafe | perl -pe 's/(^|-|_)([a-z])/uc($2)/ge' )
+else
+  name=$1
 fi
 
 for path in $(find . -type d -path "*LIBRARY-NAME*")
 do
-  mv $path ${path//LIBRARY-NAME/$1}
+  mv $path ${path//LIBRARY-NAME/$name}
 done
 
 for path in $(find . -type f ! -path "./.*" ! -name "*.sh" ! -name ".DS_Store")
 do
-  sed -i "" -e "s/LIBRARY-NAME/$1/g" $path
+  sed -i "" -e "s/LIBRARY-NAME/$name/g" $path
   if [[ $path =~ "LIBRARY-NAME" ]]
   then
-    mv $path ${path//LIBRARY-NAME/$1}
+    mv $path ${path//LIBRARY-NAME/$name}
   fi
 done
 
